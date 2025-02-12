0b55af2fb83cef79fe45c39fa6d83346e6bb3fa1
damian-bots <tosu0791@gmail.com>
2025-02-13 04:17:10 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -6,18 +6,9 @@ import logging
 logger = logging.getLogger("Mafia Bot Config")
 
 def read_tokens():
-    try:
-        with open(resource_path(os.path.join('data','token.txt')), 'r') as file:
-            lines = [line.strip() for line in file.readlines()]
-            if len(lines) < 3:
-                logger.error("token.txt must contain at least three lines: Telegram token, Random.org API key, and Maintainer Telegram ID.")
-                exit(1)
+    
             TOKEN = "7470264967:AAHTssrBhJ2IyNOpzdCGMTlaANqf8B2Je-k"
             RANDOM_ORG_API_KEY = "478ea3fb-9b67-492e-ac72-961cf70b5650"
             MAINTAINER_ID = "6848223695"
-            return TOKEN, RANDOM_ORG_API_KEY, MAINTAINER_ID
-    except FileNotFoundError:
-        logger.error("token.txt not found.")
-        exit(1)
 
 TOKEN, RANDOM_ORG_API_KEY, MAINTAINER_ID = read_tokens()
