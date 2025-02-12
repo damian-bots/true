75c020a72be99fa98519a35dbd347f15c93cbf9d
damian-bots <tosu0791@gmail.com>
2025-02-13 04:17:42 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,10 +5,7 @@ import logging
 
 logger = logging.getLogger("Mafia Bot Config")
 
-def read_tokens():
     
             TOKEN = "7470264967:AAHTssrBhJ2IyNOpzdCGMTlaANqf8B2Je-k"
             RANDOM_ORG_API_KEY = "478ea3fb-9b67-492e-ac72-961cf70b5650"
             MAINTAINER_ID = "6848223695"
-
-TOKEN, RANDOM_ORG_API_KEY, MAINTAINER_ID = read_tokens()
